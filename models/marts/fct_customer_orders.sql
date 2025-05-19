835d4ab6aee8ee11a9c8bbf1b4fc65a3c427a623
theHinneh <thehinneh1@gmail.com>
2025-05-19 01:36:40 -0400

    remove aliases

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -35,23 +35,23 @@ paid_orders as (
         orders.user_id as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
+        completed_payments.total_amount_paid,
+        completed_payments.payment_finalized_date,
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
     from orders
-    left join completed_payments p on orders.id = p.order_id
-    left join customers c on orders.user_id = c.id
+    left join completed_payments on orders.id = completed_payments.order_id
+    left join customers on orders.user_id = customers.id
 ),
 
 customer_orders as (
     select
-        c.id as customer_id,
+        customers.id as customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(orders.id) as number_of_orders
-    from customers c
-    left join orders on orders.user_id = c.id
+    from customers
+    left join orders on orders.user_id = customers.id
     group by 1
 ),
 
@@ -59,17 +59,17 @@ customer_orders as (
 
 final as (
     select
-        p.*,
-        row_number() over (order by p.order_id) as transaction_seq,
-        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+        paid_orders.*,
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
         case
-            when c.first_order_date = p.order_placed_at then 'new'
+            when customer_orders.first_order_date = paid_orders.order_placed_at then 'new'
             else 'return'
         end as nvsr,
-        sum(total_amount_paid) over (partition by p.customer_id order by p.order_placed_at) as customer_lifetime_value,
-        c.first_order_date as fdos
-    from paid_orders p
-    left join customer_orders c using (customer_id)
+        sum(total_amount_paid) over (partition by paid_orders.customer_id order by paid_orders.order_placed_at) as customer_lifetime_value,
+        customer_orders.first_order_date as fdos
+    from paid_orders
+    left join customer_orders using (customer_id)
 )
 
 -- Final Select
